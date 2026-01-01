75e35c0c37e11e5811cd20bcc706112c073cf3d9
rachitpoudel101 <rachit300poudel@gmail.com>
2026-01-01 11:38:02 +0545

    Added the mixins ani project inititions

---
+++ apps/department/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from apps.common.models import SoftDeleteModelMixin, BaseTimeStampModelMixin, BaseAuditModelMixin
+
+class Department(SoftDeleteModelMixin, BaseTimeStampModelMixin, BaseAuditModelMixin):
+    """Department/Division"""
+    company = models.ForeignKey('users.Company', on_delete=models.CASCADE, related_name='departments')
+    name = models.CharField(max_length=200)
+    code = models.CharField(max_length=50)
+    description = models.TextField(blank=True)
+    parent_department = models.ForeignKey('self', null=True, blank=True, on_delete=models.SET_NULL)
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        db_table = 'department'
+        unique_together = ['company', 'code']
+
+    def __str__(self):
+        return self.name
+
+
+class Designation(SoftDeleteModelMixin, BaseTimeStampModelMixin, BaseAuditModelMixin):
+    """Job positions/titles"""
+    company = models.ForeignKey('users.Company', on_delete=models.CASCADE, related_name='designations')
+    title = models.CharField(max_length=200)
+    code = models.CharField(max_length=50)
+    description = models.TextField(blank=True)
+    level = models.IntegerField(default=1, help_text="Hierarchy level")
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        db_table = 'designation'
+        unique_together = ['company', 'code']
+
+    def __str__(self):
+        return self.title
